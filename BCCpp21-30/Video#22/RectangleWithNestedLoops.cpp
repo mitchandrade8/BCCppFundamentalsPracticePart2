2e1d6bdf1ec0056ab4fe2458df5a29d9840f28f8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-22 13:38:15 -0700

    --Start: of Rectangle Example with Nested Loops

---
+++ BCCpp21-30/Video#22/RectangleWithNestedLoops.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+int main() {
+
+    int rows;
+    int columns;
+    char symbol;
+
+    std::cout << "How many rows?: ";
+    std::cin >> rows;
+
+    std::cout << "How many columns?: ";
+    std::cin >> columns;
+
+    std::cout << "Enter a symbol to use: ";
+    std::cin >> symbol;
+
+    
+
+    return 0;
+
+}
