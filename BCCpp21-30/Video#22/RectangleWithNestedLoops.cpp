838cca898b31f22782c55120d5bb19f5b62f8eb2
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-22 13:41:36 -0700

    --Completed Rectangle Nested Loop

--- BCCpp21-30/Video#22/RectangleWithNestedLoops.cpp
+++ BCCpp21-30/Video#22/RectangleWithNestedLoops.cpp
@@ -16,7 +16,13 @@ int main() {
     std::cout << "Enter a symbol to use: ";
     std::cin >> symbol;
 
-    
+    for (int i = 1; i <= rows; i++) {
+
+        for (int j = 1; j <= columns; j++) {
+            std::cout << symbol;
+        }
+        std::cout << '\n';
+    }
 
     return 0;
 
