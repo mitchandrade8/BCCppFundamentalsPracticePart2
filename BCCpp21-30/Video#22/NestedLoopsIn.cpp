6770c8a1df900acb5eaeeff33d03635be3fb0da6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-22 13:33:05 -0700

    --Completed Nested Loop example in Cpp

--- BCCpp21-30/Video#22/NestedLoopsIn.cpp
+++ BCCpp21-30/Video#22/NestedLoopsIn.cpp
@@ -3,7 +3,26 @@
 
 int main() {
 
+    // MARK: - Key Notes
+        // A 'Nested Loop' = a loop inside another loop
+
+    // Simple loop that goes up to 10:
+
+    /* for (int i = 0; i <= 10; i++) {
+        std::cout << i << ' ';
+    } */
+
+    // Example with loop up to 10 3 seperate times:
+
+    for (int i = 1; i <= 3; i++) {
+        // Named out index for each loop i and then j 
+        for (int j = 0; j <= 10; j++) {
+            std::cout << j << ' ';
+        }
+        // Puts other iterations on a new line
+        std::cout << '\n';
+    }
 
     return 0;
-    
+
 }
