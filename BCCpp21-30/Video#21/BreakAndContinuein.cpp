29dec0cc917231246b19a31b869c5d7a40767cee
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-22 13:13:05 -0700

    first commit

---
+++ BCCpp21-30/Video#21/BreakAndContinuein.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main() {
+
+    // MARK: - Key Notes
+        // 'BREAK' = break out of a loop
+        // 'CONTINUE' = skip current iteration
+
+    
+
+
+    return 0;
+    
+}
