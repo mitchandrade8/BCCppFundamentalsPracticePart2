07ef0419f35528aec6817a7da2c9b944ea62f16d
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-22 13:22:13 -0700

    --Start: of Nested Loops in Cpp

--- BCCpp21-30/Video#21/BreakAndContinuein.cpp
+++ BCCpp21-30/Video#21/BreakAndContinuein.cpp
@@ -7,7 +7,12 @@ int main() {
         // 'BREAK' = break out of a loop
         // 'CONTINUE' = skip current iteration
 
-    
+    for (int i = 1; i <= 20; i++) {
+        if(i == 13) {
+            continue;
+        }
+        std::cout << i << '\n';
+    }
 
 
     return 0;
