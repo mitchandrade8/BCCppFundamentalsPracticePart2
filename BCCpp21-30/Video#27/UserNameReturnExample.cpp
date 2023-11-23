8070b51a2fdeed4aa50d91ef8eb509bc80933aed
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-23 07:28:17 -0700

    --Function to concate 2 strings

--- BCCpp21-30/Video#27/UserNameReturnExample.cpp
+++ BCCpp21-30/Video#27/UserNameReturnExample.cpp
@@ -1,8 +1,17 @@
 
 #include <iostream>
 
+std::string concatString(std::string string1, std::string string2);
+
 int main() {
 
+    std::string firstName = "Everyday";
+    std::string lastName = "I'm Hustlin'";
+
     return 0;
-    
+
+}
+
+std::string concatString(std::string string1, std::string string2) {
+    return string1 + " " + string2;
 }
