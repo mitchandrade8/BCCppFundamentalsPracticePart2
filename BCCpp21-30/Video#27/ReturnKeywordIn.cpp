7cdf60abf59b4bac0c91097e4682c43432481fe8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-23 07:17:58 -0700

    --added area and volume

--- BCCpp21-30/Video#27/ReturnKeywordIn.cpp
+++ BCCpp21-30/Video#27/ReturnKeywordIn.cpp
@@ -1,14 +1,33 @@
 
 #include <iostream>
 
+double square(double length);
+double cube(double length);
+
 int main() {
 
     // MARK: - Key Notes
         // Return = return a value back to the spot
         //          where you called the encompassing function
     
-    
+    double length = 6.0;
+    double area = square(length);
+    double volume = cube(length);
+
+    std::cout << "Area: " << area << "cm^2\n";
+    std::cout << "Volume: " << volume << "cm^3\n";
 
     return 0;
+
+}
+
+double square(double length) {
+    return length * length;
+    //double result = length * length;
+    //return result;
+}
+
+double cube(double length) {
+    return length * length * length;
 
 }
