20fe78bc2bc52b2a64ad43dda29957f2502e28b0
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-23 06:59:16 -0700

    --Added an argument to the function

--- BCCpp21-30/Video#26/CreateUserDefinedFunctionsIn.cpp
+++ BCCpp21-30/Video#26/CreateUserDefinedFunctionsIn.cpp
@@ -1,9 +1,29 @@
 
 #include <iostream>
 
+
+
+void happyBirthday(std::string name);
+
+
 int main() {
 
+    // MARK: - Key Note
+        // Function = a block of reusable code
+    
+    std::string name = "Mitch";
+
+    happyBirthday(name);
 
     return 0;
-    
+
+}
+
+void happyBirthday(std::string name) {
+
+    std::cout << "Happy Birthday to " << name << '\n';
+    std::cout << "Happy Birthday to " << name << '\n';
+    std::cout << "Happy Birthday dear " << name << '\n';
+    std::cout << "Happy Birthday to " << name << '\n';
+
 }
